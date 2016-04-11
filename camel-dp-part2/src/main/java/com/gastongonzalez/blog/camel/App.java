40db37e4d95fb98d4b3a283ee8d5c05e78d32201
Gaston Gonzalez <gg@headwire.com>
2016-04-11 10:42:46 -0700

    Initial implementation

---
+++ camel-dp-part2/src/main/java/com/gastongonzalez/blog/camel/App.java
@@ -0,0 +1,38 @@
+package com.gastongonzalez.blog.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.gson.GsonDataFormat;
+import org.apache.camel.component.solr.SolrConstants;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App
+{
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args ) throws Exception
+    {
+        CamelContext context = new DefaultCamelContext();
+
+        final GsonDataFormat gsonDataFormat = new GsonDataFormat();
+        gsonDataFormat.setUnmarshalType(Products.class);
+
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception
+            {
+                from("file:data/solr?noop=true")
+                    .unmarshal(gsonDataFormat)
+                        .setBody().simple("${body.products}")
+                            .split().body()
+                                .setHeader(SolrConstants.OPERATION, constant(SolrConstants.OPERATION_ADD_BEAN))
+                                .to("solrCloud://localhost:8983/solr/gettingstarted?zkHost=localhost:9983&collection=gettingstarted");
+            }
+        });
+
+        context.start();
+        Thread.sleep(10000);
+        context.stop();
+    }
+}
